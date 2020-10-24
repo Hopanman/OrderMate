9dba7b56735bd0f9e1afcd62fe07080c2e07c8ac
Hopanman <soredoom9@naver.com>
2020-10-24 15:41:58 +0900

    create splash

---
+++ app/src/main/java/hopanman/android/ordermate/LauncherActivity.java
@@ -0,0 +1,28 @@
+package hopanman.android.ordermate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler(Looper.myLooper());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, 1500);
+    }
+}
