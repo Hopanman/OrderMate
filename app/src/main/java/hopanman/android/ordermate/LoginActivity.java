ebb240a56a9c8d822a13172a7b9fef1335ea5221
Hopanman <soredoom9@naver.com>
2020-10-25 19:43:30 +0900

    launcher-icon update

--- app/src/main/java/hopanman/android/ordermate/LoginActivity.java
+++ app/src/main/java/hopanman/android/ordermate/LoginActivity.java
@@ -1,14 +1,50 @@
 package hopanman.android.ordermate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private LoginCustomerFragment customerFragment;
+    private LoginStoreFragment storeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        customerFragment = new LoginCustomerFragment();
+        storeFragment = new LoginStoreFragment();
+
+        TabLayout tabs = findViewById(R.id.login_tabs);
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = customerFragment;
+                } else if (position == 1) {
+                    selected = storeFragment;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.login_fragment, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
