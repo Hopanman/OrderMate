ca0961cc487697f5caf2701e8dafc5bdc0d3dd9a
Hopanman <soredoom9@naver.com>
2020-10-27 22:49:09 +0900

    store-login update

--- app/src/main/java/hopanman/android/ordermate/LoginStoreFragment.java
+++ app/src/main/java/hopanman/android/ordermate/LoginStoreFragment.java
@@ -7,16 +7,45 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import hopanman.android.ordermate.databinding.FragmentLoginStoreBinding;
 
 
 public class LoginStoreFragment extends Fragment {
 
+    private TextInputEditText editText;
+    private TextInputEditText editText2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)FragmentLoginStoreBinding.inflate(inflater).getRoot();
+        editText = rootView.findViewById(R.id.id_edittext);
+        editText2 = rootView.findViewById(R.id.password_edittext);
 
-        return FragmentLoginStoreBinding.inflate(inflater).getRoot();
+        Button button = rootView.findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editText.getText().toString();
+                if (email.equals("") || email == null) {
+                    editText.requestFocus();
+                    editText.setError("아이디를 입력해주세요.");
+                    return;
+                }
+
+                String password = editText2.getText().toString();
+                if (password.equals("") || password == null) {
+                    editText2.requestFocus();
+                    editText2.setError("비밀번호를 입력해주세요.");
+                    return;
+                }
+            }
+        });
+
+        return rootView;
     }
 }
