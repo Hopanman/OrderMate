906db0275a700afabd6767f99572909780a72489
Hopanman <soredoom9@naver.com>
2020-11-06 17:07:31 +0900

    store-fragment setting

--- app/src/main/java/hopanman/android/ordermate/StoreActivity.java
+++ app/src/main/java/hopanman/android/ordermate/StoreActivity.java
@@ -1,12 +1,21 @@
 package hopanman.android.ordermate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class StoreActivity extends AppCompatActivity {
+
+    private StoreMainFragment mainFragment;
+    private StoreOrderFragment orderFragment;
+    private StoreMenuFragment menuFragment;
+    private StoreReviewFragment reviewFragment;
+    private StoreSettingFragment settingFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,37 @@ public class StoreActivity extends AppCompatActivity {
         MaterialToolbar toolbar = findViewById(R.id.store_toolbar);
         toolbar.setTitle(getIntent().getStringExtra("storeName"));
         setSupportActionBar(toolbar);
+
+        mainFragment = new StoreMainFragment();
+        orderFragment = new StoreOrderFragment();
+        menuFragment = new StoreMenuFragment();
+        reviewFragment = new StoreReviewFragment();
+        settingFragment = new StoreSettingFragment();
+
+        BottomNavigationView navigation = findViewById(R.id.store_navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_store_order_list:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.store_fragment, mainFragment).commit();
+                        return true;
+                    case R.id.menu_store_last_order:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.store_fragment, orderFragment).commit();
+                        return true;
+                    case R.id.menu_store_menu_mgm:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.store_fragment, menuFragment).commit();
+                        return true;
+                    case R.id.menu_store_review_mgm:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.store_fragment, reviewFragment).commit();
+                        return true;
+                    case R.id.menu_store_setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.store_fragment, settingFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
