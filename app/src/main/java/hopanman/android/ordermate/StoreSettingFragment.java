061a5db330da887354edbf2d15c0a5dec3e4680a
Hopanman <soredoom9@naver.com>
2020-11-11 21:09:10 +0900

    store-setting-fragment update

--- app/src/main/java/hopanman/android/ordermate/StoreSettingFragment.java
+++ app/src/main/java/hopanman/android/ordermate/StoreSettingFragment.java
@@ -1,5 +1,7 @@
 package hopanman.android.ordermate;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +10,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+
+import hopanman.android.ordermate.databinding.FragmentStoreSettingBinding;
+
 
 public class StoreSettingFragment extends Fragment {
+
+    private ViewGroup logoutRow;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store_setting, container, false);
+        ViewGroup rootView = (ViewGroup)FragmentStoreSettingBinding.inflate(inflater).getRoot();
+
+        logoutRow = rootView.findViewById(R.id.logout);
+        logoutRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+                builder.setMessage(R.string.logout_dialog_body).setPositiveButton(R.string.dialog_positive_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAuth.signOut();
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
+                        startActivity(intent);
+                        getActivity().overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out);
+                        getActivity().finish();
+                    }
+                }).setNegativeButton(R.string.dialog_negative_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+            }
+        });
+
+        return rootView;
     }
 }
