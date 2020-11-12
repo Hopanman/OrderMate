df8567eb19f6865569e72b93cd8bdee62d13fcf8
Hopanman <soredoom9@naver.com>
2020-11-12 22:30:22 +0900

    store-setting-fragment update

--- app/src/main/java/hopanman/android/ordermate/StoreSettingFragment.java
+++ app/src/main/java/hopanman/android/ordermate/StoreSettingFragment.java
@@ -1,16 +1,22 @@
 package hopanman.android.ordermate;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 import hopanman.android.ordermate.databinding.FragmentStoreSettingBinding;
@@ -18,6 +24,7 @@ import hopanman.android.ordermate.databinding.FragmentStoreSettingBinding;
 
 public class StoreSettingFragment extends Fragment {
 
+    private ViewGroup passwordRow;
     private ViewGroup logoutRow;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
@@ -25,6 +32,39 @@ public class StoreSettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)FragmentStoreSettingBinding.inflate(inflater).getRoot();
+
+        passwordRow = rootView.findViewById(R.id.password_change);
+        passwordRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.view_dialog_edittext, null);
+                TextInputLayout layout = view.findViewById(R.id.edittext_layout);
+                layout.setHint("현재 비밀번호");
+                TextInputEditText editText = layout.findViewById(R.id.edittext);
+                editText.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                InputMethodManager inputManager = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                editText.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        editText.requestFocus();
+                        inputManager.showSoftInput(editText, 0);
+                    }
+                }, 200);
+
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+                builder.setTitle(R.string.password_change).setView(view).setPositiveButton(R.string.dialog_positive_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("StoreSettingFragment", "비밀번호 변경");
+                    }
+                }).setNegativeButton(R.string.dialog_negative_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setCancelable(false).show();
+            }
+        });
 
         logoutRow = rootView.findViewById(R.id.logout);
         logoutRow.setOnClickListener(new View.OnClickListener() {
