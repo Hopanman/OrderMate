5585c238414cfd8f92400697f4c417f13acf332b
Hopanman <soredoom9@naver.com>
2020-12-18 23:28:31 +0900

    naver-map setting

--- app/src/main/java/hopanman/android/ordermate/CustomerMainFragment.java
+++ app/src/main/java/hopanman/android/ordermate/CustomerMainFragment.java
@@ -2,19 +2,97 @@ package hopanman.android.ordermate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
 
-public class CustomerMainFragment extends Fragment {
+
+public class CustomerMainFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        
-        return inflater.inflate(R.layout.fragment_customer_main, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_customer_main, container, false);
+
+        mapView = rootView.findViewById(R.id.customer_main_map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setMinZoom(5.98);
+        naverMap.setMaxZoom(19.0);
+        naverMap.setExtent(new LatLngBounds(new LatLng(31.94, 123.74), new LatLng(38.82, 131.87)));
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setScaleBarEnabled(false);
+        uiSettings.setZoomControlEnabled(false);
+        uiSettings.setLogoGravity(Gravity.TOP|Gravity.RIGHT);
+        uiSettings.setTiltGesturesEnabled(false);
+        uiSettings.setRotateGesturesEnabled(false);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        float density = displayMetrics.density;
+        uiSettings.setLogoMargin(0, (int)(10 * density), (int)(10 * density), 0);
     }
 }
